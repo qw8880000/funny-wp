a5ff9a641c031601335b5c473dfb09e83595f1f6
qw8880000 <qw8880000@126.com>
2019-07-11 22:24:21 +0800

    sass and sass compiler

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Funny_WP
+ * @since 1.0.0
+ */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="profile" href="https://gmpg.org/xfn/11" />
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header class="">
+
+</header>
+
+</body>
+</html>

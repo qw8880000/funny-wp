4c82a99b4949901a99d5a11055bfd583a145eafa
qw8880000 <qw8880000@126.com>
2019-07-17 22:18:28 +0800

    add content.php

--- index.php
+++ index.php
@@ -16,6 +16,27 @@
 ?>
 <?php get_header(); ?>
 
-<section id="main" class="main">adflak</section>
+<section id="main" class="main">
+    <?php if ( have_posts() ) {
+
+        // Load posts loop.
+        while ( have_posts() ) {
+            the_post();
+            get_template_part( 'template-parts/content/content' );
+        }
+
+        // Previous/next page navigation.
+        // twentynineteen_the_posts_navigation();
+
+    } else {
+
+        // If no content, include the "No posts found" template.
+        // get_template_part( 'template-parts/content/content', 'none' );
+
+    }
+    ?>
+
+    </main><!-- .site-main -->
+</section>
 
 <?php get_footer(); ?>

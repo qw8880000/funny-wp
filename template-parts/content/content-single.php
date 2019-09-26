8488c14847fa577e4dd8f6cf7f9342a4115a3e6c
qw8880000 <qw8880000@126.com>
2019-09-26 22:16:52 +0800

    add post-meta.php

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -3,27 +3,7 @@
         <?php the_title(); ?>
     </h1>
 
-    <div class="post-meta">
-        <?php if ( has_tag() ) : ?>
-            <div class="post-tags">
-                <span class="iconfont icon-tag"></span>
-                <?php the_tags(''); ?>
-            </div>
-        <?php endif ?>
-
-        <?php if ( has_category() ) : ?>
-            <div class="post-categories">
-                <span class="iconfont icon-book-mark"></span>
-                <?php the_category(', ', '', $post_id=get_the_ID()); ?>
-            </div>
-        <?php endif ?>
-
-        <div class="post-create_time">
-            <span class="iconfont icon-calendar"></span>
-            <?php /*the_time('Y-m-d H:i:s'); */?>
-            <?php the_time('Y-m-d'); ?>
-        </div>
-    </div>
+    <?php get_template_part( 'template-parts/post-meta' ); ?>
 
     <div class="post-content"><?php the_content(); ?></div>
 
---
+++ template-parts/post-meta.php
@@ -0,0 +1,21 @@
+<div class="post-meta">
+    <?php if ( has_tag() ) : ?>
+        <div class="post-tags">
+            <span class="iconfont icon-tag"></span>
+            <?php the_tags(''); ?>
+        </div>
+    <?php endif ?>
+
+    <?php if ( has_category() ) : ?>
+        <div class="post-categories">
+            <span class="iconfont icon-book-mark"></span>
+            <?php the_category(', ', '', $post_id=get_the_ID()); ?>
+        </div>
+    <?php endif ?>
+
+    <div class="post-create_time">
+        <span class="iconfont icon-calendar"></span>
+        <?php /*the_time('Y-m-d H:i:s'); */?>
+        <?php the_time('Y-m-d'); ?>
+    </div>
+</div>
